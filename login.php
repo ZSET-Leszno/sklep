40feff55400e0605eb27e8e6beaacfc8cb51701c
mikiprzewo <miki.przewo@gmail.com>
2023-03-06 13:33:36 +0100

    skubany

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-    $user_password = mysqli_real_escape_string($db_conn, $_POST["haslo"]);
+    $user_password = mysqli_real_escape_string($db_conn, $_POST["password"]);
     $user_email = mysqli_real_escape_string($db_conn, $_POST["email"]);
     $query_login = mysqli_query($db_conn, "SELECT * FROM users WHERE user_email ='$user_email'");
     if(mysqli_num_rows($query_login) > 0) {
